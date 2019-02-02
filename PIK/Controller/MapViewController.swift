fd19b61aa4c5f154f3a3f339f601767353a07925
PurpleRiver <idel.yusupoff@yandex.ru>
2019-02-02 13:23:57 +0500

    Custom info view for google maps

--- PIK/Controller/MapViewController.swift
+++ PIK/Controller/MapViewController.swift
@@ -1,16 +1,20 @@
 import UIKit
 import GoogleMaps
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, GMSMapViewDelegate {
 
     @IBOutlet fileprivate weak var mapView: GMSMapView!
     var products: Product = Product()
-    
+    let iconImage = UIImageView()
     // MARK: - View Controller Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        iconImage.image = UIImage(named: "pikIcon")
+        iconImage.layer.cornerRadius = 25
+        iconImage.layer.masksToBounds = true
+        
         setCameraPosition()
+        mapView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -42,8 +46,29 @@ class MapViewController: UIViewController {
         let marker = GMSMarker()
         marker.appearAnimation = .pop
         marker.position = position
-        marker.title = products.getName()
-        marker.snippet = ""
+//        marker.title = products.getName()
+        
+        marker.icon = iconImage.image
+//        marker.snippet = ""
         marker.map = mapView
+        
+    }
+  
+    
+    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+        
+        let customInfoWindow = Bundle.main.loadNibNamed("CustomInfoWindow", owner: self, options: nil)!.first as! CustomInfoWindow
+        
+        customInfoWindow.name.text = products.getName()
+        
+        let imgUrl = NSURL(string: products.getImage())
+        
+        if imgUrl != nil {
+            let data = NSData(contentsOf: imgUrl! as URL)
+            customInfoWindow.thumbImage.image = UIImage(data: data! as Data)
+        }
+  //      customInfoWindow.thumbImage.image = UIImage(named: products.getImage())
+        
+        return customInfoWindow
     }
 }
---
+++ PIK/View/CustomInfoWindow.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class CustomInfoWindow: UIView {
+
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var thumbImage: UIImageView!
+    
+}
